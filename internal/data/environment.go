819c4ed71fe8e6898a83519b2a3664db2b6bc3bb
Casey Entzi <casey.entzi@silverblueprints.com>
2020-05-11 19:01:13 -0500

    adding metadata back to the environment object

--- internal/data/environment.go
+++ internal/data/environment.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"gitlab.unanet.io/devops/eve/pkg/errors"
+	"gitlab.unanet.io/devops/eve/pkg/json"
 )
 
 type Environment struct {
-	ID    int    `db:"id"`
-	Name  string `db:"name"`
-	Alias string `db:"alias"`
+	ID       int       `db:"id"`
+	Name     string    `db:"name"`
+	Alias    string    `db:"alias"`
+	Metadata json.Text `db:"metadata"`
 }
 
 type Environments []Environment
