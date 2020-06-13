10481c924bd3dd111833a04426a60866df368a2d
Casey Entzi <casey.entzi@silverblueprints.com>
2020-06-12 23:11:42 -0500

    removing logging, did something stupid

--- pkg/eve/message.go
+++ pkg/eve/message.go
@@ -5,10 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"go.uber.org/zap"
-
 	"gitlab.unanet.io/devops/eve/pkg/errors"
-	"gitlab.unanet.io/devops/eve/pkg/log"
 	"gitlab.unanet.io/devops/eve/pkg/s3"
 )
 
@@ -43,7 +40,6 @@ func UnMarshalNSDeploymentFromS3LocationBody(ctx context.Context, cd CloudDownlo
 
 func MarshalNSDeploymentPlanToS3LocationBody(ctx context.Context, cu CloudUploader, plan *NSDeploymentPlan) ([]byte, error) {
 	nsDeploymentJson, err := json.Marshal(plan)
-	log.Logger.Info(fmt.Sprintf("#################### nsdeploymentJson"), zap.String("plan", string(nsDeploymentJson)))
 	if err != nil {
 		return nil, errors.Wrap(err)
 	}
