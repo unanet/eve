b8ae7628dd7343dc5023443db070dfabd81a4b1b
Troy Sampson <troy.j.sampson@gmail.com>
2020-07-19 19:59:56 -0600

    remove migration

--- migrations/00003_feed_alias.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE feed ADD alias varchar(25) default '';
-UPDATE feed SET (alias) = 'int' WHERE id in (1,2);
-UPDATE feed SET (alias) = 'stage' WHERE id in (5,6);
-UPDATE feed SET (alias) = 'prod' WHERE id in (7,8);
-
